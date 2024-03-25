fb3595523500439c7b9b7ea6c76f5ec38333e6c3
hojoon123 <rhzn5512@naver.com>
2024-03-26 01:35:30 +0900

    Initial commit

---
+++ src/components/Footers/SimpleFooter.js
@@ -0,0 +1,178 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+// reactstrap components
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+class SimpleFooter extends React.Component {
+  render() {
+    return (
+      <>
+        <footer className=" footer">
+          <Container>
+            <Row className=" row-grid align-items-center mb-5">
+              <Col lg="6">
+                <h3 className=" text-primary font-weight-light mb-2">
+                  너의 꿈을 응원해!
+                </h3>
+                <h4 className=" mb-0 font-weight-light">
+                  넌 할 수 있어! 자신을 믿어!
+                </h4>
+              </Col>
+              <Col className="text-lg-center btn-wrapper" lg="6">
+              <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="github"
+                  href="https://github.com/hojoon123"
+                  id="tooltip495507257"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-github" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip495507257">
+                  호준
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="github"
+                  href="https://github.com/hyohyo12"
+                  id="tooltip495507258"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-github" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip495507258">
+                  동효
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="github"
+                  href="https://github.com/mingd00"
+                  id="tooltip495507259"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-github" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip495507259">
+                  민지
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="github"
+                  href="https://github.com/YongBin123"
+                  id="tooltip495507260"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-github" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip495507260">
+                  용빈
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="github"
+                  href="https://github.com/wnsdyd1"
+                  id="tooltip495507261"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-github" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip495507261">
+                  준용
+                </UncontrolledTooltip>
+              </Col>
+            </Row>
+            <hr />
+            <Row className=" align-items-center justify-content-md-between">
+              <Col md="6">
+                <div className=" copyright">
+                  © {new Date().getFullYear()}{" "}
+                  <a
+                    href="https://github.com/orgs/Team-Fusion-X"
+                    target="_blank"
+                  >
+                    Team-Fusion-X
+                  </a>
+                  .
+                </div>
+              </Col>
+              <Col md="6">
+                <Nav className=" nav-footer justify-content-end">
+                  <NavItem>
+                    <NavLink
+                      href="https://github.com/orgs/Team-Fusion-X"
+                      target="_blank"
+                    >
+                      Team-Fusion-X
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="https://github.com/orgs/Team-Fusion-X"
+                      target="_blank"
+                    >
+                      About Us
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="https://github.com/orgs/Team-Fusion-X"
+                      target="_blank"
+                    >
+                      Blog
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="https://github.com/orgs/Team-Fusion-X"
+                      target="_blank"
+                    >
+                      MIT License
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default SimpleFooter;
---
+++ src/index.js
@@ -0,0 +1,55 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
+
+import Index from "views/Index.js";
+import FullAnalysis from "views/examples/analysis/FullAnalysis.js";
+import IntensiveAnalysis from "views/examples/analysis/IntensiveAnalysis.js";
+import MapAnalysis from "views/examples/analysis/MapAnalysis.js";
+import Landing from "views/examples/Landing.js";
+import Login from "views/examples/Login.js";
+import Profile from "views/examples/Profile.js";
+import Register from "views/examples/Register.js";
+import ChatBot from './views/examples/chatBot/chatBotPage.js';
+import ChatBotTest from './views/examples/chatBot/chatBot.js';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Index />} />
+      <Route path="/landing-page" exact element={<Landing />} />
+      <Route path="/full-analysis-page" exact element={<FullAnalysis />} />
+      <Route path="/intensive-analysis-page" exact element={<IntensiveAnalysis />} />
+      <Route path="/map-analysis-page" exact element={<MapAnalysis />} />
+      <Route path="/login-page" exact element={<Login />} />
+      <Route path="/profile-page" exact element={<Profile />} />
+      <Route path="/register-page" exact element={<Register />} />
+      <Route path="/chat-bot" element={<ChatBot />} />
+      <Route path="/chat-bot-test" element={<ChatBotTest />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </BrowserRouter>
+);
