4e2a5bc37ad551e23ab52cf2c6550cb6330c78f3
hojoon123 <rhzn5512@naver.com>
2024-03-31 22:55:19 +0900

    [Feat] 성적 데이터 삽입 & 불러오기 함수 구현
    백엔드 기능 미구현으로 현재 주석처리 합니다.

--- src/views/IndexSections/record.js
+++ src/views/IndexSections/record.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import {
   Input,
   Row,
-  Col
+  Col,
+  Button
 } from "reactstrap";
+import axios from 'axios'; // Axios 추가
 
 function Record() {
   const [activeTab, setActiveTab] = useState('1-1');
@@ -15,6 +17,38 @@ function Record() {
   const [curriculum, setCurriculum] = useState([]);
   const [subjects, setSubjects] = useState({});
   const [selectedCurriculum, setSelectedCurriculum] = useState('');
+  const [selectedSubject, setSelectedSubject] = useState('');
+  const [credits, setCredits] = useState('');
+  const [rawScore, setRawScore] = useState('');
+  const [subjectAverage, setSubjectAverage] = useState('');
+  const [standardDeviation, setStandardDeviation] = useState('');
+  const [studentsNumber, setStudentsNumber] = useState('');
+  const [rank, setRank] = useState('');
+
+  const handleCurriculumChange = (event) => {
+    setSelectedCurriculum(event.target.value);
+  };
+  const handleSubjectChange = (event) => {
+    setSelectedSubject(event.target.value);
+  };
+  const handleCreditsChange = (event) => {
+    setCredits(event.target.value);
+  };
+  const handleRawScoreChange = (event) => {
+    setRawScore(event.target.value);
+  };
+  const handleSubjectAverageChange = (event) => {
+    setSubjectAverage(event.target.value);
+  };
+  const handleStandardDeviationChange = (event) => {
+    setStandardDeviation(event.target.value);
+  };
+  const handleStudentsNumberChange = (event) => {
+    setStudentsNumber(event.target.value);
+  };
+  const handleRankChange = (event) => {
+    setRank(event.target.value);
+  };
 
   let scoreData = {
     '1-1': [
@@ -25,6 +59,31 @@ function Record() {
       { curriculum: '국어', subject: '국어 2', credits: 4, rawScore: 90, subjectAverage: 85, standardDeviation: 6, studentsNumber: 60, rank: 8 },
     ],
     // Add data for other tabs
+  };
+
+  const handleAddScore = () => {
+    let [schoolYear, schoolTerm] = activeTab.split('-');
+    let newScore = {
+      school_year: parseInt(schoolYear),
+      school_term: parseInt(schoolTerm),
+      curriculum: selectedCurriculum,
+      subject: selectedSubject,
+      credits: parseInt(credits),
+      rawScore: parseInt(rawScore),
+      subjectAverage: parseInt(subjectAverage),
+      standardDeviation: parseInt(standardDeviation),
+      studentsNumber: parseInt(studentsNumber),
+      rank: parseInt(rank)
+    };
+    console.log(newScore)
+    // axios.post('your_api_endpoint_here', newScore)
+    // .then(response => {
+    //   console.log('성적이 추가되었습니다.');
+    //   // 성적이 성공적으로 추가되었을 때 해야 할 작업 추가
+    // })
+    // .catch(error => {
+    //   console.error('성적 추가 중 오류가 발생했습니다.', error);
+    // });
   };
 
   useEffect(() => {
@@ -38,11 +97,41 @@ function Record() {
       '영어': ['영어1', '영어2'],
     };
     setSubjects(subjectsData);
-  }, []);
 
-  const handleCurriculumChange = (event) => {
-    setSelectedCurriculum(event.target.value);
-  };
+    // 주소 + 세션에 로그인한 ID로 넣기만 하면 끝
+    // 서버에서 세션으로 접속된 user_id를 기반으로 score 데이터를 가져오는 Get 요청 수행
+    // axios.get('your_api_endpoint_here')  
+    // .then(response => {
+    //   // 서버에서 받아온 데이터를 scoreData로 설정
+    //   const receivedData = response.data; // 서버에서 받아온 데이터
+    //   const formattedData = {}; // 정제된 데이터를 저장할 객체
+    //   receivedData.forEach((item, index) => {
+    //     // 서버에서 받아온 데이터를 newScore와 동일한 형식으로 변환하여 formattedData에 추가
+    //     const { school_year, school_term, curriculum, subject, credits, rawScore, subjectAverage, standardDeviation, studentsNumber, rank } = item;
+    //     const newScore = {
+    //       school_year: parseInt(school_year),
+    //       school_term: parseInt(school_term),
+    //       curriculum,
+    //       subject,
+    //       credits: parseInt(credits),
+    //       rawScore: parseInt(rawScore),
+    //       subjectAverage: parseInt(subjectAverage),
+    //       standardDeviation: parseInt(standardDeviation),
+    //       studentsNumber: parseInt(studentsNumber),
+    //       rank: parseInt(rank)
+    //     };
+    //     if (formattedData[school_year + '-' + school_term]) {
+    //       formattedData[school_year + '-' + school_term].push(newScore);
+    //     } else {
+    //       formattedData[school_year + '-' + school_term] = [newScore];
+    //     }
+    //   });
+    //   setScoreData(formattedData);
+    // })
+    // .catch(error => {
+    //   console.error('데이터를 가져오는 중 오류가 발생했습니다.', error);
+    // });
+  }, []);
 
   return (
     <div>
@@ -111,7 +200,7 @@ function Record() {
                     </select>
                   </th>
                   <th scope="col">
-                    <select>
+                    <select value={selectedSubject} onChange={handleSubjectChange}>
                       <option value="">과목 선택</option>
                       {selectedCurriculum && subjects[selectedCurriculum] && subjects[selectedCurriculum].map((subject, index) => (
                         <option key={index} value={subject}>{subject}</option>
@@ -123,6 +212,8 @@ function Record() {
                       id="credits"
                       placeholder=""
                       type="text"
+                      value={credits}
+                      onChange={handleCreditsChange}
                     />
                   </th>
                   <th scope="col">
@@ -130,6 +221,8 @@ function Record() {
                       id="rawScore"
                       placeholder=""
                       type="text"
+                      value={rawScore}
+                      onChange={handleRawScoreChange}
                     />
                   </th>
                   <th scope="col">
@@ -137,6 +230,8 @@ function Record() {
                       id="subjectAverage"
                       placeholder=""
                       type="text"
+                      value={subjectAverage}
+                      onChange={handleSubjectAverageChange}
                     />
                   </th>
                   <th scope="col">
@@ -144,13 +239,17 @@ function Record() {
                       id="standardDeviation"
                       placeholder=""
                       type="text"
+                      value={standardDeviation}
+                      onChange={handleStandardDeviationChange}
                     />
                   </th>
                   <th scope="col">
                     <Input
-                      id="stuentsNumber"
+                      id="studentsNumber"
                       placeholder=""
                       type="text"
+                      value={studentsNumber}
+                      onChange={handleStudentsNumberChange}
                     />
                   </th>
                   <th scope="col">
@@ -158,11 +257,16 @@ function Record() {
                       id="rank"
                       placeholder=""
                       type="text"
+                      value={rank}
+                      onChange={handleRankChange}
                     />
                   </th>
                 </tr>
               </tbody>
             </table>
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+              <Button color="primary" onClick={handleAddScore}>성적 추가</Button>
+            </div>
           </div>
         ))}
       </div>
