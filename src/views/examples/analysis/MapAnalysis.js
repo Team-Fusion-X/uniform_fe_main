defa3c069d79897a50b3e28635d75f317675d6ba
mingd00 <rkdalswl403@naver.com>
2024-04-12 22:17:00 +0900

    [feat] 마커에 로고, 홈페이지 주소 추가
    - json파일에 image, home_url 스크래핑 후 데이터 추가
    - 화면에 json data 랜더링(imgae, home_url)

--- src/views/examples/analysis/MapAnalysis.js
+++ src/views/examples/analysis/MapAnalysis.js
@@ -2,7 +2,6 @@ import React from "react";
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
 import UMap from "./UMap";
-import Overlay from "./Overlay";
 
 class Landing extends React.Component {
   state = {};
@@ -28,7 +27,7 @@ class Landing extends React.Component {
 
           {/* 작업공간 시작 */}
           <section className="section" style={{ paddingTop: '1rem', paddingBottom: '3rem' }}>
-            <Overlay />
+            <UMap />
           </section>
           {/* 작업공간 끝 */}
 
