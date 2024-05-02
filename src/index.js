499ead8cfc2186ec381fe6699cf75a37175a3738
hojoon123 <rhzn5512@naver.com>
2024-05-02 17:15:50 +0900

    [Feat] 집중 분석 모드 search 컴포넌트 추가
    집중 분석 모드 Search.js 파일 추가
    세션을 통한 최적화 및 서버 부하 감소

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
 import Index from "views/Index.js";
 import FullAnalysis from "views/examples/analysis/FullAnalysis.js";
-import IntensiveAnalysis from "views/examples/analysis/IntensiveAnalysis.js";
+import IntensiveAnalysis from "views/examples/analysis/IntensiveAnalysis/IntensiveAnalysis.js";
 import MapAnalysis from "views/examples/analysis/MapAnalysis.js";
 import Landing from "views/examples/Landing.js";
 import Login from "views/examples/Login.js";
