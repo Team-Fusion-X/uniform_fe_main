8e3188326c742da9d1b60580663c9ba399315ff6
hojoon123 <rhzn5512@naver.com>
2024-05-14 03:19:33 +0900

    [Feat] 집중분석 구현
    집중분석 페이지 구현 완료
    ai 서버 proxy 작업 완료
    ai 서버 500에러
    내가 알기론 하스팟으로만 가능했던 걸로 기억... 이러면 어떻게 보여주지?

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,13 +9,20 @@ module.exports = function(app) {
       pathRewrite: { '^/api/8482': '/api' }, // URL에서 /api/8482 부분을 /api로 재작성
     })
   );
-
   app.use(
     '/api/8582', // 8582에 대한 요청을 처리
     createProxyMiddleware({
       target: 'http://orion.mokpo.ac.kr:8582', // 프록시 대상 서버 주소
       changeOrigin: true,
       pathRewrite: { '^/api/8582': '/api' }, // URL에서 /api/8582 부분을 /api로 재작성
+    })
+  );
+  app.use(
+    '/ai/predict', // ai 서버
+    createProxyMiddleware({
+      target: 'http://114.70.92.44:11030', // 프록시 대상 서버 주소
+      changeOrigin: true,
+      pathRewrite: { '^/ai/predict': '/predict' },
     })
   );
 };
