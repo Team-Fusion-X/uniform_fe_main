836fbe4c46cbffbf67c78694c748283cb73a02b4
YongBin123 <134280533+YongBin123@users.noreply.github.com>
2024-05-31 19:50:05 +0900

    [Feat] 전체분석 모달창 및 알림창 추가, 기능 개선
    - 작업 내용
    1. 로딩 중일 때는 로딩 모달창, 로딩이 끝나면 분석 완료 모달창이 뜨도록 함.
    2. 여유, 적절, 도전, 위험 중 해당 데이터가 없을 경우 알림창으로 안내할 수 있도록 추가

--- src/views/examples/analysis/Full/FullAnalModal.js
+++ src/views/examples/analysis/Full/FullAnalModal.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './FullAnalModal.css';
 
-const Modal = ({ closeModal }) => {
+const Modal = ({ closeModal, isLoading }) => {
   return (
     <div className="fullAnalModalOverlay">
-      <div className="fullAnalModalContent">
+      <div className={`fullAnalModalContent ${isLoading ? 'loading' : ''}`}>
         <div className="fullAnalModalHeader">
-          <h2 className='fullAnalModalTitle'>분석 완료!</h2>
+          <h2 className='fullAnalModalTitle'>{isLoading ? '로딩 중...' : '분석 완료!'}</h2>
           <button className="fullAnalModalCloseButton" onClick={closeModal}>
             &times;
           </button>
         </div>
         <div className="fullAnalModalBody">
           <p className='fullAnalModalDescription'>
-            분석 결과는 여유, 적절, 도전, 위험으로 나뉘어지며, 각각의 버튼을 클릭하면 결과를 확인하실 수 있습니다!
+            {isLoading ? '분석을 진행 중입니다. 잠시만 기다려 주세요...' : '분석 결과는 여유, 적절, 도전, 위험으로 나뉘어지며, 각각의 버튼을 클릭하면 결과를 확인하실 수 있습니다!'}
           </p>
         </div>
       </div>
